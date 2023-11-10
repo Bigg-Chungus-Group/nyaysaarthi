9df78fb33090db88d6ee4d1c2e923ef18266e519
Anurag <anuragsawant@duck.com>
2023-11-10 23:05:07 +0530

    Fixed Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
