948f789b39b93d825ab610f2e94fad0d1378752a
Bharat Sharma <bharatsharma98971@gmail.com>
2023-11-11 17:54:49 +0530

    Completed profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import Profile_setup from './pages/Profile_setup/Profile_setup';
 import { ToastContainer } from "react-toastify";
-import "../firebaseConfig"
+import "../firebaseConfig";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<h1>Hello</h1>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup/>}/>
+        <Route path="/profile" element={<Profile_setup/>}/>
       </Routes>
       <ToastContainer
         position="bottom-right"
